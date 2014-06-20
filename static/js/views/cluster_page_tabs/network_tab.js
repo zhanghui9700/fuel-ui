openstack-archive/fuel-web

Refactoring for error dialogs
Closes-Bug:#1297158

Change-Id: Ib3c457e06357d8b2d1b165fdc12eb85c27785a0e

nailgun/static/js/views/cluster_page_tabs/network_tab.js
@@ -69,7 +69,10 @@ function(utils, models, commonViews, dialogViews, networkTabTemplate, networkTem
             };
             task.save({}, options)
                 .fail(_.bind(function() {
-                    utils.showErrorDialog({title: 'Network verification'});
+                    utils.showErrorDialog({
+                        title: $.t('cluster_page.network_tab.verify_networks.verification_error.title'),
+                        message: $.t('cluster_page.network_tab.verify_networks.verification_error.saving_warning')
+                    });
                     this.$('.verify-networks-btn').prop('disabled', false);
                 }, this))
                 .always(_.bind(function() {
@@ -115,7 +118,7 @@ function(utils, models, commonViews, dialogViews, networkTabTemplate, networkTem
                         }
                     }, this))
                     .fail(_.bind(function() {
-                        utils.showErrorDialog({title: 'Networks'});
+                        utils.showErrorDialog({title: $.t('cluster_page.network_tab.verify_networks.verification_error.title')});
                         this.defaultButtonsState(false);
                         this.model.fetch();
                         this.model.fetchRelated('tasks');

zhanghui9700/fuel-ui

static/js/views/cluster_page_tabs/network_tab.js
@@ -84,7 +84,10 @@ function(utils, models, commonViews, dialogViews, networkTabTemplate, networkTem
             };
             task.save({}, options)
                 .fail(_.bind(function() {
-                    utils.showErrorDialog({title: $.t('cluster_page.network_tab.network_verification')});
+                    utils.showErrorDialog({
+                        title: $.t('cluster_page.network_tab.verify_networks.verification_error.title'),
+                        message: $.t('cluster_page.network_tab.verify_networks.verification_error.start_verification_warning')
+                    });
                     this.calculateButtonsState();
                 }, this))
                 .always(_.bind(function() {
@@ -126,7 +129,7 @@ function(utils, models, commonViews, dialogViews, networkTabTemplate, networkTem
                         }
                     }, this))
                     .fail(_.bind(function() {
-                        utils.showErrorDialog({title: $.t('cluster_page.network_tab.title')});
+                        utils.showErrorDialog({title: $.t('cluster_page.network_tab.verify_networks.verification_error.title')});
                         this.calculateButtonsState();
                         this.model.fetch();
                         this.model.fetchRelated('tasks');