openstack-archive/fuel-web

Patching UI - changes for downgrade
Closes-Bug:#1331464

Change-Id: I233606c7c6c572f8913ee843d48fba03c77a1929

nailgun/static/js/views/dialogs.js
@@ -238,7 +238,7 @@ function(require, utils, models, simpleMessageTemplate, discardChangesDialogTemp
             }
         },
         render: function() {
-            this.constructor.__super__.render.call(this, {action: this.action});
+            this.constructor.__super__.render.call(this, {cluster: this.model, action: this.action, releases:  this.releases});
             return this;
         }
     });

zhanghui9700/fuel-ui

static/js/views/dialogs.js
@@ -241,7 +241,7 @@ function(require, utils, models, baseDialogTemplate, discardChangesDialogTemplat
             }
         },
         render: function() {
-            this.constructor.__super__.render.call(this, {action: this.action});
+            this.constructor.__super__.render.call(this, {cluster: this.model, action: this.action, isDowngrade: this.isDowngrade});
             return this;
         }
     });