openstack-archive/fuel-web

[Intern] Advanced tests for Settings tab
Related to blueprint ui-functional-tests-with-intern

Change-Id: I2bbb3d440e72f6007df6aa2eb8ffd7cae60ce7f6

nailgun/static/tests/functional/pages/common.js
@@ -172,10 +172,12 @@ define([
                         }, false)
                     });
             },
-            setInputElementValue: function(element, value) {
-                element.clearValue()
-                    .type(value)
-                    .end();
+            fill: function(cssSelector, value) {
+                return this.remote
+                    .findByCssSelector(cssSelector)
+                        .clearValue()
+                        .type(value)
+                        .end();
             },
             isElementEnabled: function(cssSelector, message) {
                 return this.remote
@@ -192,6 +194,22 @@ define([
                         .isEnabled()
                         .then(function(isEnabled) {
                             return assert.isFalse(isEnabled, message);
+                        })
+                        .end();
+            },
+            doesElementExist: function(cssSelector, message) {
+                return this.remote
+                    .findAllByCssSelector(cssSelector)
+                        .then(function(elements) {
+                            return assert.equal(elements.length, 1, message);
+                        })
+                        .end();
+            },
+            doesntElementExist: function(cssSelector, message) {
+                return this.remote
+                    .findAllByCssSelector(cssSelector)
+                        .then(function(elements) {
+                            return assert.equal(elements.length, 0, message);
                         })
                         .end();
             }

nailgun/static/tests/functional/test_cluster_network.js
@@ -138,7 +138,7 @@ define([
             'Testing cluster networks: Save button interactions': function() {
                 return this.remote
                     .then(function() {
-                        return common.setInputElementValue(cidrElement, '240.0.1.0/25');
+                        return common.fill('.storage input[name=cidr]', '240.0.1.0/25');
                     })
                     // apply btn
                     .then(function() {
@@ -149,7 +149,7 @@ define([
                             .end();
                     })
                     .then(function() {
-                        return common.setInputElementValue(cidrElement, initialValue);
+                        return common.fill('.storage input[name=cidr]', initialValue);
                     })
                     .then(function() {
                         return networksPage.isApplyButtonEnabled()

zhanghui9700/fuel-ui

static/tests/functional/pages/common.js
@@ -172,10 +172,12 @@ define([
                         }, false)
                     });
             },
-            setInputElementValue: function(element, value) {
-                element.clearValue()
-                    .type(value)
-                    .end();
+            setInputValue: function(cssSelector, value) {
+                return this.remote
+                    .findByCssSelector(cssSelector)
+                        .clearValue()
+                        .type(value)
+                        .end();
             },
             isElementEnabled: function(cssSelector, message) {
                 return this.remote
@@ -192,6 +194,22 @@ define([
                         .isEnabled()
                         .then(function(isEnabled) {
                             return assert.isFalse(isEnabled, message);
+                        })
+                        .end();
+            },
+            elementExists: function(cssSelector, message) {
+                return this.remote
+                    .findAllByCssSelector(cssSelector)
+                        .then(function(elements) {
+                            return assert.equal(elements.length, 1, message);
+                        })
+                        .end();
+            },
+            elementNotExists: function(cssSelector, message) {
+                return this.remote
+                    .findAllByCssSelector(cssSelector)
+                        .then(function(elements) {
+                            return assert.equal(elements.length, 0, message);
                         })
                         .end();
             }

static/tests/functional/test_cluster_network.js
@@ -138,7 +138,7 @@ define([
             'Testing cluster networks: Save button interactions': function() {
                 return this.remote
                     .then(function() {
-                        return common.setInputElementValue(cidrElement, '240.0.1.0/25');
+                        return common.setInputValue('.storage input[name=cidr]', '240.0.1.0/25');
                     })
                     // apply btn
                     .then(function() {
@@ -149,7 +149,7 @@ define([
                             .end();
                     })
                     .then(function() {
-                        return common.setInputElementValue(cidrElement, initialValue);
+                        return common.setInputValue('.storage input[name=cidr]', initialValue);
                     })
                     .then(function() {
                         return networksPage.isApplyButtonEnabled()