openstack-archive/fuel-web

Allow interfaces configuration for provisioned node
Closes-Bug: #1518391

Change-Id: Ia122d2f3d9130b2650648de3e31814360c8b0e67

nailgun/static/utils.js
@@ -327,6 +327,13 @@ define([
                 linkStart = isMirantisIso ? 'https://docs.mirantis.com/openstack/fuel/fuel-' :
                     'https://docs.fuel-infra.org/openstack/fuel/fuel-';
             return linkStart + release + '/' + link;
+        },
+        getNodeList: function(options) {
+            var nodeIds = utils.deserializeTabOptions(options.screenOptions[0]).nodes,
+                ids = nodeIds ? nodeIds.split(',').map((id) => parseInt(id, 10)) : [],
+                models = require('models'),
+                nodes = new models.Nodes(options.cluster.get('nodes').getByIds(ids));
+            if (nodes.length == ids.length) return nodes;
         }
 };
 

nailgun/static/views/cluster_page_tabs/nodes_tab_screens/edit_node_disks_screen.js
@@ -30,7 +30,6 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
 
     var EditNodeDisksScreen = React.createClass({
         mixins: [
-            ComponentMixins.nodeConfigurationScreenMixin,
             ComponentMixins.backboneMixin('cluster', 'change:status change:nodes sync'),
             ComponentMixins.backboneMixin('nodes', 'change sync'),
             ComponentMixins.backboneMixin('disks', 'reset change'),
@@ -38,7 +37,7 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
         ],
         statics: {
             fetchData: function(options) {
-                var nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                var nodes = utils.getNodeList(options);
 
                 if (!nodes || !nodes.areDisksConfigurable()) {
                     return $.Deferred().reject();
@@ -65,6 +64,10 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
         },
         componentWillMount: function() {
             this.updateInitialData();
+        },
+        isLocked: function() {
+            return !!this.props.cluster.task({group: 'deployment', active: true}) ||
+                !_.all(this.props.nodes.invoke('areDisksConfigurable'));
         },
         updateInitialData: function() {
             this.setState({initialDisks: _.cloneDeep(this.props.nodes.at(0).disks.toJSON())});
@@ -192,7 +195,9 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
                         <div className='col-xs-12 page-buttons content-elements'>
                             <div className='well clearfix'>
                                 <div className='btn-group'>
-                                    {this.renderBackToNodeListButton()}
+                                    <a className='btn btn-default' href={'#cluster/' + this.props.cluster.id + '/nodes'} disabled={this.state.actionInProgress}>
+                                        {i18n('cluster_page.nodes_tab.back_to_nodes_button')}
+                                    </a>
                                 </div>
                                 {!locked && !!this.props.disks.length &&
                                     <div className='btn-group pull-right'>

nailgun/static/views/cluster_page_tabs/nodes_tab_screens/edit_node_interfaces_screen.js
@@ -36,7 +36,6 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
 
     var EditNodeInterfacesScreen = React.createClass({
         mixins: [
-            ComponentMixins.nodeConfigurationScreenMixin,
             ComponentMixins.backboneMixin('interfaces', 'change reset update'),
             ComponentMixins.backboneMixin('cluster'),
             ComponentMixins.backboneMixin('nodes', 'change reset update'),
@@ -45,7 +44,7 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
         statics: {
             fetchData: function(options) {
                 var cluster = options.cluster,
-                    nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                    nodes = utils.getNodeList(options);
 
                 if (!nodes || !nodes.areInterfacesConfigurable()) {
                     return $.Deferred().reject();
@@ -93,6 +92,10 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
         },
         componentDidMount: function() {
             this.validate();
+        },
+        isLocked: function() {
+            return !!this.props.cluster.task({group: 'deployment', active: true}) ||
+                !_.all(this.props.nodes.invoke('areInterfacesConfigurable'));
         },
         initInterfacesMap: function() {
             this.setState({interfacesMap: _.range(0, this.props.interfaces.length)});
@@ -522,7 +525,9 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
                     <div className='col-xs-12 page-buttons content-elements'>
                         <div className='well clearfix'>
                             <div className='btn-group'>
-                                {this.renderBackToNodeListButton()}
+                                <a className='btn btn-default' href={'#cluster/' + this.props.cluster.id + '/nodes'} disabled={this.state.actionInProgress}>
+                                    {i18n('cluster_page.nodes_tab.back_to_nodes_button')}
+                                </a>
                             </div>
                             {!locked &&
                                 <div className='btn-group pull-right'>

nailgun/static/views/cluster_page_tabs/nodes_tab_screens/edit_nodes_screen.js
@@ -20,17 +20,16 @@ define(
     'react',
     'models',
     'utils',
-    'component_mixins',
     'views/cluster_page_tabs/nodes_tab_screens/node_list_screen'
 ],
-function($, _, React, models, utils, ComponentMixins, NodeListScreen) {
+function($, _, React, models, utils, NodeListScreen) {
     'use strict';
 
     var EditNodesScreen = React.createClass({
         statics: {
             fetchData: function(options) {
                 var cluster = options.cluster,
-                    nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                    nodes = utils.getNodeList(options);
 
                 if (!nodes) {
                     return $.Deferred().reject();

zhanghui9700/fuel-ui

static/utils.js
@@ -47,6 +47,13 @@ define([
                 return option.split(':');
             }));
         },
+        getNodeListFromTabOptions: function(options) {
+            var nodeIds = utils.deserializeTabOptions(options.screenOptions[0]).nodes,
+                ids = nodeIds ? nodeIds.split(',').map((id) => parseInt(id, 10)) : [],
+                models = require('models'),
+                nodes = new models.Nodes(options.cluster.get('nodes').getByIds(ids));
+            if (nodes.length == ids.length) return nodes;
+        },
         linebreaks: function(text) {
             return text.replace(/\n/g, '<br/>');
         },

static/views/cluster_page_tabs/nodes_tab_screens/edit_node_disks_screen.js
@@ -30,7 +30,6 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
 
     var EditNodeDisksScreen = React.createClass({
         mixins: [
-            ComponentMixins.nodeConfigurationScreenMixin,
             ComponentMixins.backboneMixin('cluster', 'change:status change:nodes sync'),
             ComponentMixins.backboneMixin('nodes', 'change sync'),
             ComponentMixins.backboneMixin('disks', 'reset change'),
@@ -38,7 +37,7 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
         ],
         statics: {
             fetchData: function(options) {
-                var nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                var nodes = utils.getNodeListFromTabOptions(options);
 
                 if (!nodes || !nodes.areDisksConfigurable()) {
                     return $.Deferred().reject();
@@ -65,6 +64,10 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
         },
         componentWillMount: function() {
             this.updateInitialData();
+        },
+        isLocked: function() {
+            return !!this.props.cluster.task({group: 'deployment', active: true}) ||
+                !_.all(this.props.nodes.invoke('areDisksConfigurable'));
         },
         updateInitialData: function() {
             this.setState({initialDisks: _.cloneDeep(this.props.nodes.at(0).disks.toJSON())});
@@ -192,7 +195,9 @@ function($, _, i18n, Backbone, React, utils, models, ComponentMixins, controls) 
                         <div className='col-xs-12 page-buttons content-elements'>
                             <div className='well clearfix'>
                                 <div className='btn-group'>
-                                    {this.renderBackToNodeListButton()}
+                                    <a className='btn btn-default' href={'#cluster/' + this.props.cluster.id + '/nodes'} disabled={this.state.actionInProgress}>
+                                        {i18n('cluster_page.nodes_tab.back_to_nodes_button')}
+                                    </a>
                                 </div>
                                 {!locked && !!this.props.disks.length &&
                                     <div className='btn-group pull-right'>

static/views/cluster_page_tabs/nodes_tab_screens/edit_node_interfaces_screen.js
@@ -36,7 +36,6 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
 
     var EditNodeInterfacesScreen = React.createClass({
         mixins: [
-            ComponentMixins.nodeConfigurationScreenMixin,
             ComponentMixins.backboneMixin('interfaces', 'change reset update'),
             ComponentMixins.backboneMixin('cluster'),
             ComponentMixins.backboneMixin('nodes', 'change reset update'),
@@ -45,7 +44,7 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
         statics: {
             fetchData: function(options) {
                 var cluster = options.cluster,
-                    nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                    nodes = utils.getNodeListFromTabOptions(options);
 
                 if (!nodes || !nodes.areInterfacesConfigurable()) {
                     return $.Deferred().reject();
@@ -93,6 +92,10 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
         },
         componentDidMount: function() {
             this.validate();
+        },
+        isLocked: function() {
+            return !!this.props.cluster.task({group: 'deployment', active: true}) ||
+                !_.all(this.props.nodes.invoke('areInterfacesConfigurable'));
         },
         initInterfacesMap: function() {
             this.setState({interfacesMap: _.range(0, this.props.interfaces.length)});
@@ -522,7 +525,9 @@ function($, _, Backbone, React, i18n, utils, models, dispatcher, dialogs, contro
                     <div className='col-xs-12 page-buttons content-elements'>
                         <div className='well clearfix'>
                             <div className='btn-group'>
-                                {this.renderBackToNodeListButton()}
+                                <a className='btn btn-default' href={'#cluster/' + this.props.cluster.id + '/nodes'} disabled={this.state.actionInProgress}>
+                                    {i18n('cluster_page.nodes_tab.back_to_nodes_button')}
+                                </a>
                             </div>
                             {!locked &&
                                 <div className='btn-group pull-right'>

static/views/cluster_page_tabs/nodes_tab_screens/edit_nodes_screen.js
@@ -20,17 +20,16 @@ define(
     'react',
     'models',
     'utils',
-    'component_mixins',
     'views/cluster_page_tabs/nodes_tab_screens/node_list_screen'
 ],
-function($, _, React, models, utils, ComponentMixins, NodeListScreen) {
+function($, _, React, models, utils, NodeListScreen) {
     'use strict';
 
     var EditNodesScreen = React.createClass({
         statics: {
             fetchData: function(options) {
                 var cluster = options.cluster,
-                    nodes = ComponentMixins.nodeConfigurationScreenMixin.getNodeList(options);
+                    nodes = utils.getNodeListFromTabOptions(options);
 
                 if (!nodes) {
                     return $.Deferred().reject();