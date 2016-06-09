5bdca4dc3db46cfaacc62557e52499e55de242ea
Julia Aranovich <jkirnosova@mirantis.com>
2016-06-09 12:08:19 +0300

    Fix locked HealthCheck tab warning
    Closes-Bug: #1574733
    
    Change-Id: Ie245877e39bc532ef17f3d3671b26af593bd9832

--- static/views/cluster_page_tabs/dashboard_tab.js
+++ static/views/cluster_page_tabs/dashboard_tab.js
@@ -1155,7 +1155,7 @@ var ClusterInfo = React.createClass({
               </div>
             </div>
             {this.renderClusterInfoFields()}
-            {(cluster.get('status') === 'operational') &&
+            {cluster.isHealthCheckAvailable() &&
               <div className='go-to-healthcheck'>
                 {i18n(ns + 'healthcheck')}
                 <a href={'#cluster/' + cluster.id + '/healthcheck'}>
