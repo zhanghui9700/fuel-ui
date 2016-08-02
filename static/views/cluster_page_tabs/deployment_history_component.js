2aaa8e447a3b033108e71942cfc9980a40961643
Kate Pimenova <kpimenova@mirantis.com>
2016-08-02 19:30:44 +0300

    Functional tests added for deployment history
    Implements blueprint ui-deployment-history
    
    Change-Id: I6368cde98ca29625e7ec28b869d984322943f837

--- static/views/cluster_page_tabs/deployment_history_component.js
+++ static/views/cluster_page_tabs/deployment_history_component.js
@@ -236,7 +236,7 @@ var DeploymentHistory = React.createClass({
                   (filter) => <MultiSelectControl
                     {...filter}
                     key={filter.name}
-                    className={utils.classNames('filter-control', ['filter-by-' + filter.name])}
+                    className={utils.classNames('filter-control', 'filter-by-' + filter.name)}
                     onChange={_.partial(this.changeFilter, filter.name)}
                     isOpen={openFilter === filter.name}
                     toggle={_.partial(this.toggleFilter, filter.name)}
