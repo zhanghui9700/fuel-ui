e8a4071867086b2a2dc5e046cb2c5d9f1fd07ca7
Kate Pimenova <kpimenova@mirantis.com>
2016-08-05 19:27:24 +0300

    Functional tests added for workflows
    Implements blueprint ui-custom-graph
    
    Change-Id: I2ced91a78f372393b2a1f34e7abf28cae0776e0b

--- static/views/cluster_page_tabs/workflows_tab.js
+++ static/views/cluster_page_tabs/workflows_tab.js
@@ -180,7 +180,7 @@ var WorkflowsTab = React.createClass({
                       (filter) => <MultiSelectControl
                         {...filter}
                         key={filter.name}
-                        className={utils.classNames('filter-control', ['filter-by-' + filter.name])}
+                        className={utils.classNames('filter-control', 'filter-by-' + filter.name)}
                         onChange={_.partial(this.changeFilter, filter.name)}
                         isOpen={openFilter === filter.name}
                         toggle={_.partial(this.toggleFilter, filter.name)}
