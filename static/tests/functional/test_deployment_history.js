a141353f25e1418494b820847afe748b19be8e57
Vitaly Kramskikh <vkramskikh@mirantis.com>
2016-08-31 19:22:49 +0300

    Show node names instead of node IDs
    It's not practical to show node IDs in the UI for deployment
    history - we don't use them anywhere else in the UI.
    
    Change-Id: Iea64667e1fbc6c08a64709d48dc2ee2305235f2c
    Partial-Bug: #1616967

--- static/tests/functional/test_deployment_history.js
+++ static/tests/functional/test_deployment_history.js
@@ -93,7 +93,7 @@ define([
             'Two buttons are presented and disabled in zoom control'
           )
           .assertElementsExist(
-            '.deployment-timeline .node-names div',
+            '.deployment-timeline .node-names > div',
             2,
             'Two timelines are shown - for master and slave node'
           )
@@ -135,8 +135,6 @@ define([
           .waitForElementDeletion('.dashboard-block .progress', 40000);
       },
       'Test deployment history tab of finished deployment': function() {
-        var deploymentTasksNumber;
-
         return this.remote
           .then(function() {
             return clusterPage.goToTab('History');
@@ -177,25 +175,10 @@ define([
           )
           .clickByCssSelector('.deployment-history-toolbar .btn-filters')
           .assertElementsExist(
-            '.filters .filter-by-task_name,.filters .filter-by-node_id,.filters .filter-by-status',
-            3,
-            'Three filters are presented: filter by Task Name, by Node ID, and by Task status'
-          )
-          .findAllByCssSelector('.history-table table tbody tr')
-            .then(function(elements) {
-              deploymentTasksNumber = elements.length;
-            })
-            .end()
-          .clickByCssSelector('.filters .filter-by-node_id')
-          .clickByCssSelector('.popover-content .checkbox-group input[name="master"]')
-          .findAllByCssSelector('.history-table table tbody tr')
-            .then(function(elements) {
-              return assert.isTrue(
-                deploymentTasksNumber > elements.length,
-                'Filter by node ID works and shows tasks only for master node'
-              );
-            })
-            .end();
+            '.filters .filter-by-task_name, .filters .filter-by-status',
+            2,
+            'Two filters are presented: filter by Task Name and by Task status'
+          );
       },
       'Test deployment history tab after second cluster deployment': function() {
         this.timeout = 100000;
