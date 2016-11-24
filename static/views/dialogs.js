138e8fed7a11506ce635dbe872053a73b698510a
Julia Aranovich <jkirnosova@mirantis.com>
2016-11-24 15:11:05 +0300

    Fix node attributes render
    Closes-Bug: #1644531
    
    Change-Id: I650be85389eb938a5b446f6d7e17b5e7ae4fa55e

--- static/views/dialogs.js
+++ static/views/dialogs.js
@@ -1667,6 +1667,7 @@ export var ShowNodeInfoDialog = React.createClass({
               {... {sectionName, settingsToDisplay, cluster, configModels}}
               key={sectionName}
               initialAttributes={initialNodeAttributes}
+              getValueAttribute={nodeAttributes.getValueAttribute}
               onChange={_.partial(this.onNodeAttributesChange, sectionName)}
               settings={nodeAttributes}
               locked={
